3587915f9cdb8b2b5a4d0ccf06ccd35d80a2283e
hunterbugs <79522220+hunterbugs@users.noreply.github.com>
2023-06-22 23:57:21 -0700

    practice js

---
+++ validate.js
@@ -0,0 +1,12 @@
+function login() {
+    if(document.getElementById("username").value == "testing") {
+        alert("testing");
+    } else {
+        alert("JPG");
+    }
+   
+}
+
+function validateInputFromFirstPage (){
+
+}
